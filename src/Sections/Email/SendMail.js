f898685e2620374d084339a59e37af6e69598eba
Kimjeju <overrainbow0315@gmail.com>
2023-12-10 22:27:33 +0900

    메일 붙이기

---
+++ src/Sections/Email/SendMail.js
@@ -0,0 +1,189 @@
+import emailjs from 'emailjs-com';
+import { useState } from 'react';
+import styled from "styled-components";
+import Swal from "sweetalert2";
+
+
+//뒷배경 어둡게 해주기
+const Background = styled.div`
+    position : fixed;
+    top : 0;
+    left : 0;
+    width : 100vw;
+    height : 100vh;
+    background-color : rgba(0,0,0,0.8);
+    z-index : 100;
+`
+
+//모달 최상단 위치 시켜주기
+const OpenContainer = styled.div`
+    width: 20rem;
+    height : 20rem;
+    background-color : black;
+
+    //최상단 위치
+    z-index : 999;
+
+    //가운데 위치 시키기
+    position : absolute;
+    top : 50%;
+    left : 50%;
+    transform : translate(-50%, -50%);
+
+    border : 1px solid white;
+    transition : 0.5s;
+
+    display : flex;
+    flex-direction : column;
+
+    &:hover{
+        box-shadow: 10px 10px white;
+    }
+`
+const InputContainer = styled.div`
+      display : flex;
+      flex-direction : column;
+      padding : 5px;
+`
+const Label = styled.label`
+      color : white;
+      margin-top : 20px;
+`
+
+//닫기 버튼
+const Close = styled.button`
+    // 버튼 오른쪽 최상단에 위치하도록 만들어주기
+    position : absolute;
+    top : 10px;
+    right : 10px;
+    
+    //디자인 영역
+    transition : 0.5s;
+    width : 25px;
+    height : 25px;
+    transition : 0.5s;
+    background-color : #3A00F5;
+    color : white;
+    border : none;
+
+    &:hover{
+        background-color : white;
+        color : black;
+    }
+`
+
+const SendButton = styled.button`
+    witdh : 50px;
+    height : 30px;
+    border : none;
+    background-color : #01A488;
+    color : black;
+    transition : 0.5s;
+
+    &:hover{
+        background-color : #005C53;
+        color : white;
+    }
+`
+
+function SendMail({ setOpenMailModal }) {
+    const [isEmailSent, setIsEmailSent] = useState(false);
+
+    const [fromName, setFromName] = useState("");
+    const [fromEmail, setFromEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [message, setMessage] = useState("");
+
+    const onChangeFromName = (e) => {
+        setFromName(e.target.value);
+    };
+
+    const onChangeFromEmail = (e) => {
+        setFromEmail(e.target.value);
+    };
+    const onChangePhone = (e) => {
+        setPhone(e.target.value);
+    };
+    const onChangeMessage = (e) => {
+        setMessage(e.target.value);
+    };
+
+    const onClose = (e) => {
+        setOpenMailModal(false);
+    };
+
+    const sendVerificationEmail = () => {
+        // 이메일 보내기
+        // 여기서 정의해야하는 것은 위에서 만든 메일 템플릿에 지정한 변수({{ }})에 대한 값을 담아줘야한다.
+        const templateParams = {
+            to_name: "김건휘",
+            from_name: fromName,
+            from_email: fromEmail,
+            phone: phone,
+            message: message
+        };
+
+        emailjs
+            .send(
+                'service_sy1xtvp', // 서비스 ID
+                'template_kddvesk', // 템플릿 ID
+                templateParams,
+                'LOg6PzOiGcWr3UC6-' // public-key
+            )
+            .then((response) => {
+                Swal.fire({
+                    text: "이메일이 성공적으로 보내졌습니다.",
+                    icon: "success",
+                    title: "메일 보내기",
+                    showCancelButton: true,
+                    confirmButtonText: "확인",
+                })
+                setIsEmailSent(true);
+                setOpenMailModal(false);
+                // 이메일 전송 성공 처리 로직 추가
+            })
+            .catch((error) => {
+                alert('이메일 보내기 실패:', error);
+                // 이메일 전송 실패 처리 로직 추가
+            });
+    };
+
+    const handleVerification = () => {
+        sendVerificationEmail();
+    };
+
+    return (
+        <Background>
+            <OpenContainer>
+                <Close onClick={onClose}>X</Close>
+
+                <InputContainer>
+                    <Label>성함</Label>
+                    <input type='text' onChange={onChangeFromName} value={fromName} />
+
+                    <Label>이메일</Label>
+                    <input type='email' onChange={onChangeFromEmail} value={fromEmail} />
+
+                    <Label>전화번호</Label>
+                    <input type='tel' onChange={onChangePhone} value={phone} />
+
+                    <Label>내용</Label>
+                    <textarea type='text' onChange={onChangeMessage} value={message} />
+
+                </InputContainer>
+
+                <h2>이메일 인증</h2>
+                {isEmailSent ? (
+                    <p>
+                        인증 이메일이 성공적으로 발송되었습니다. 이메일을
+                        확인해주세요!
+                    </p>
+                ) : (
+                    <SendButton onClick={handleVerification}>Send Mail</SendButton>
+                )}
+            </OpenContainer>
+        </Background>
+    );
+}
+
+export default SendMail;
--- src/Sections/Intro/Intro.js
+++ src/Sections/Intro/Intro.js
@@ -2,6 +2,15 @@ import { useState } from "react"
 import styled from "styled-components";
 import "../../Styles/intro.css"
 import ModalPage from "../Modal/ModalPage";
+import SendMail from "../Email/SendMail";
+
+
+const ButtonContainer = styled.div`
+    display : flex;
+    flex-direction : row;
+    width : 250px;
+    justify-content: space-between;
+`
 
 const Button = styled.button`
     background-color : #3A00F5; 
@@ -21,40 +30,72 @@ const Button = styled.button`
     }
 `
 
+
+const MailButton = styled.button`
+    background-color : white; 
+    color : black;
+    border : none;
+    width : 50px;
+    height : 50px;
+    font-size : 15px;
+    transition : 0.5s;
+    margin-top : 20px;
+    border-radius : 5px;
+    margin-right : 20px;
+
+    &:hover{
+        background-color :  #3A00F5;
+        border-radius : 25px;
+        color : black;
+    }
+`
+
 const Img = styled.img`
     witdh : 20rem;
     height : 20rem;
     object-fit: cover;
 `
 
-function Intro()
-{
+function Intro() {
     //Modal 영역
-    const [ openModal, setOpenModal ] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
 
-    function showModal(){
+    const [openMailModal, setOpenMailModal] = useState(false);
+
+
+    function showModal() {
         setOpenModal(true);
     }
+
+    function showMailModal() {
+        setOpenMailModal(true);
+    }
+
 
     return (
         <div className="intro-body">
             <div className="intro-left-body">
                 {/* 사진 들어갈 곳 */}
                 {/* <Img src="img/profile.png"/> */}
-                aaaaaaaaa
             </div>
             <div className="intro-right-body">
-                    <span id="name_text">Kim Jeju</span>
-                    <span id="name_domain">Developer BackEnd & Data Engine</span>
-                    <hr />
-                    <br/>
-                    <p id="name_introduce">안녕하세요 스스로 답을 찾아나가는 과정을 즐기는 개발자 김제주 ( 김건휘 ) 입니다.</p>
-                    <p id="name_introduce">주로 서버개발 및 인프라 구축, 운영 배포 쪽의 업무를 맡고 있습니다. </p>
-                    <p id="name_introduce">저는 오늘 날 작성한 코드를 첨삭하는 과정에서 더 나은 가독성 및 생산성을 확보하는 작업을 즐기며 개발을 하고 있습니다!</p>
-                    <p id="name_introduce">저에 대해 궁금하시다면 언제든 연락주세요 ! 감사합니다.</p>
+                <span id="name_text">Kim Jeju</span>
+                <span id="name_domain">Developer BackEnd & Data Engine</span>
+                <hr />
+                <br />
+                <p id="name_introduce">안녕하세요 스스로 답을 찾아나가는 과정을 즐기는 개발자 김제주 ( 김건휘 ) 입니다.</p>
+                <p id="name_introduce">주로 서버개발 및 인프라 구축, 운영 배포 쪽의 업무를 맡고 있습니다. </p>
+                <p id="name_introduce">저는 오늘 날 작성한 코드를 첨삭하는 과정에서 더 나은 가독성 및 생산성을 확보하는 작업을 즐기며 개발을 하고 있습니다!</p>
+                <p id="name_introduce">저에 대해 궁금하시다면 언제든 연락주세요 ! </p>
 
+                <ButtonContainer>
                     <Button onClick={showModal}>View More</Button>
                     {openModal && <ModalPage setOpenModal={setOpenModal} />}
+
+                    <MailButton onClick={showMailModal}>Send Mail</MailButton>
+                    {openMailModal && <SendMail setOpenMailModal={setOpenMailModal} />}
+
+                </ButtonContainer>
             </div>
         </div>
     )
